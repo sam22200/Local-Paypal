0bbc6826d23f29933e474ee3cb9ac475cc7116c3
COZ Samuel <coz.samuel@gmail.com>
2015-03-13 14:57:24 +0100

    Premier Commit Fix Bootstrap Product.php

--- product.php
+++ product.php
@@ -40,6 +40,7 @@ mysql_close();
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title><?php echo $product_name; ?></title>
     <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
